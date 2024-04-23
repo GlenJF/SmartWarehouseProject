5ac4ffb2f3560f56708a86e53e0891dc18985960
GlenJF <152633015+GlenJF@users.noreply.github.com>
2024-04-23 13:15:39 +0100

    lightsensor server updated

--- src/main/java/LightSensorServer.java
+++ src/main/java/LightSensorServer.java
@@ -19,22 +19,28 @@ public class LightSensorServer {
     private Server server;
 
     private void start() throws IOException {
-        int port = 5005;
+        int port = 50053;
         server = ServerBuilder.forPort(port)
                 .addService(new LightSensorServiceImpl())
                 .build()
                 .start();
-
+        System.out.println("Server started, listening on " + port);
+    //Register Server on consul
         registerToConsul();
 
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
             server.shutdown();
+            System.err.println("*** shutting down gRPC server since JVM is shutting down");
+            LightSensorServer.this.stop();
+            System.err.println("*** server shut down");
         }));
     }
 
     private void registerToConsul() {
+        System.out.println("Registering server to Consul...");
+       //Load Consul configuration from consul.properties file
         Properties props = new Properties();
-        try (FileInputStream fis = new FileInputStream("src/main/resources/consul.properties")) {
+        try (FileInputStream fis = new FileInputStream("src/main/resources/lightsensor.properties")) {
             props.load(fis);
         } catch (IOException e) {
             e.printStackTrace();
@@ -47,6 +53,7 @@ public class LightSensorServer {
         String serviceName = props.getProperty("lightsensor.service.name");
         int servicePort = Integer.parseInt(props.getProperty("lightsensor.service.port"));
 
+        //Get Host Address
         String hostAddress;
         try {
             hostAddress = InetAddress.getLocalHost().getHostAddress();
@@ -54,15 +61,17 @@ public class LightSensorServer {
             e.printStackTrace();
             return;
         }
-
+        //Create Consul Client
         ConsulClient consulClient = new ConsulClient(consulHost, consulPort);
-
+        //Define Service Details
         NewService newService = new NewService();
         newService.setName(serviceName);
         newService.setPort(servicePort);
         newService.setAddress(hostAddress);
-
+        // Register with Consul
         consulClient.agentServiceRegister(newService);
+        //Print Success Message
+        System.out.println("Server registered to Consul successfully. Host: " + hostAddress);
     }
 
     private void stop() {
